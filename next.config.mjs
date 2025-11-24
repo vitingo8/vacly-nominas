30d973e88e4e20aa5cdbb3ac8dd26816d134c806
David <davispuny@gmail.com>
2025-11-24 15:59:11 +0100

    nominas

--- next.config.mjs
+++ next.config.mjs
@@ -50,7 +50,7 @@ const nextConfig = {
           },
           {
             key: 'X-Frame-Options',
-            value: 'DENY',
+            value: 'SAMEORIGIN',
           },
           {
             key: 'X-XSS-Protection',
